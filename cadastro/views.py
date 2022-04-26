caeece9abcbc436974af340e7b2fce4cb224d7a2
makalau <marcelovictor95@hotmail.com>
2022-04-26 10:43:46 -0300

    projeto django

---
+++ cadastro/views.py
@@ -0,0 +1,8 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    hora = datetime.datetime.now()
+    html = "<html><body>Agora eh: %s.</body></html>" %hora
+    return HttpResponse(hora)
