50ec7deb13ce5863584cb9cdeb5863ef7b16223f
makalau <marcelovictor95@hotmail.com>
2022-05-03 11:05:49 -0300

    criado HomePage e static files

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,8 +1,4 @@
 ﻿from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
 
 def home(request):
-    hora = datetime.datetime.now()
-    html = "<html><body>Agora eh: %s.</body></html>" %hora
-    return HttpResponse(hora)
+    return render(request, 'cadastro/cadastro.html')
