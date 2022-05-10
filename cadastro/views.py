4574601a11d63a49cb157fb6ea34a66c9b8c4a59
makalau <marcelovictor95@hotmail.com>
2022-05-10 13:43:02 -0300

    ..

--- cadastro/views.py
+++ cadastro/views.py
@@ -5,5 +5,16 @@ def consultar(request):
     return render(request, 'cadastro/consultar.html')
     
 def cadastro(request):
-    
-    return render(request, "cadastro/cadastro.html")
+    context = {}
+    if request.method == "POST":
+        erros = {}
+        nome = request.POST.get('nome', None)
+        if nome != "Marcelo":
+            erros["nome"] = "O nome é diferente do esperado"
+        
+        if erros:
+            context['erros']  = erros 
+        else:
+            print("Salvando os dados")
+            context["msg"] = "Os dados foram salvos com sucesso"
+    return render(request, "cadastro/cadastro.html", context=context)
